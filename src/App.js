c21b1ba12009f2b9c4da5f4a44caa7b8a531db85
Tomasz Lewandowski <tomek2x1@wp.pl>
2021-10-22 21:38:34 +0200

    stworzenie pierwszego inputa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
+import styled from "styled-components";
+import TextField from "@mui/material/TextField";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <FormWrapper>
+        <form>
+          <FormHeader>Formularz reklamacji</FormHeader>
+          <TextField
+            id="outlined-password-input"
+            label="Numer zamówienia lub numer faktury"
+            type="text"
+            autoComplete="current-password"
+            fullWidth
+          />
+        </form>
+      </FormWrapper>
+    </div>
+  );
 };
 
 export default App;
+
+const FormWrapper = styled.div`
+  margin: 30px auto;
+  max-width: 800px;
+  border-radius: 15px;
+  padding: 30px;
+  background: #f5f7f7;
+`;
+
+const FormHeader = styled.h1`
+  font-size: 30px;
+  text-align: left;
+  font-family: "Roboto";
+  margin: 0 0 20px 0;
+`;
